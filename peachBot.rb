68d44f37974fd164dfc1e47f12a535915d6c89d0
Jane Greene <janegreenecasa@gmail.com>
2021-06-30 19:34:19 -0600

    completed for 3x3 only

--- peachBot.rb
+++ peachBot.rb
@@ -6,23 +6,8 @@ def displayPathtoPrincess(n,grid)
    # nested if statements to find coordinates of m and p
    #save coordinates of m & p and compare index to determine moves
    #"UP", "DOWN", "LEFT", "RIGHT"
-  puts n
-  # grid.map do |e|
-  #   if e.include? 'p'
-  #     e.index('p')
-  #     require "pry"; binding.pry
-  #   else e.include? 'm'
-  #     e.index('m')
-  #   end
-    # def coordinates(e)
-    #   each_with_index do |subarray, i|
-    #     j = subarray.index(element)
-    #     return i, j if j
-    #   end
-    #   nil
-    # end
-    # array.coordinates(m)     # => [1, 1]
-    # array.coordinates(p)     # => [2, 0]
+    p = Array.new
+    m = Array.new
     grid.each_with_index do |e,i|
       if e.include? 'p'
         py = e.index('p')
@@ -31,9 +16,34 @@ def displayPathtoPrincess(n,grid)
         my = e.index('m')
         m = i, my if my
       end
-      puts "p = #{p}" if p
-      puts "m = #{m}" if m
     end
+      rows = p[0].to_i - m[0].to_i
+      cols = p[1].to_i  - m[1].to_i
+      directions = []
+      directions.join(
+        if rows < 0
+          puts 'UP' * rows.abs()
+        else
+          puts 'DOWN' * rows.abs()
+        end)
+        directions1 = []
+        directions1.join(
+        if cols < 0
+          puts 'LEFT' * cols.abs()
+        else
+          puts 'RIGHT' * cols.abs()
+        end)
+        # Need to handle new line characters 
+        # 5
+        # ----p
+        # -----
+        # -----
+        # -----
+        # m----
+        # UPUPUPUP
+        # RIGHTRIGHTRIGHTRIGHT
+    puts directions
+    puts directions1
 end
 
 m = gets.to_i
